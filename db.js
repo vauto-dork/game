c1b0c1991e35280eee155d97c35f49d6836ad44e
Robert Szabo <robert.james.szabo@gmail.com>
2015-04-06 21:11:14 -0500

    Changing connection type

---
+++ db.js
@@ -0,0 +1,36 @@
+var mongodb = require('mongodb');
+
+var DB = function(){
+
+    // Scope
+
+    var self = this;
+
+    // Setup
+
+    self.dbServer = new mongodb.Server(process.env.OPENSHIFT_MONGODB_DB_HOST, parseInt(process.env.OPENSHIFT_MONGODB_DB_PORT));
+    self.db = new mongodb.Db(process.env.OPENSHIFT_APP_NAME, self.dbServer, {auto_reconnect: true});
+    self.dbUser = process.env.OPENSHIFT_MONGODB_DB_USERNAME;
+    self.dbPass = process.env.OPENSHIFT_MONGODB_DB_PASSWORD;
+
+    self.ipaddr  = process.env.OPENSHIFT_NODEJS_IP;
+    self.port    = parseInt(process.env.OPENSHIFT_NODEJS_PORT) || 8080;
+
+    // Logic to open a database connection. We are going to call this outside of app so it is available to all our functions inside.
+
+    self.connectDb = function(callback){
+        self.db.open(function(err, db){
+            if(err){ throw err };
+            self.db.authenticate(self.dbUser, self.dbPass, {authdb: "admin"},  function(err, res){
+                if(err){ throw err };
+                callback();
+            });
+        });
+    };
+};
+
+//make a new express app
+var database = new DB();
+
+//call the connectDb function and pass in the start server command
+database.connectDb();
