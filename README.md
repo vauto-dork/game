9c0536d36b26679df0000391639cbadfcc5436f8
Robert Szabo <robert.james.szabo@gmail.com>
2015-04-06 20:31:41 -0500

    Adding openshift env variables

--- README.md
+++ README.md
@@ -4,22 +4,22 @@ Requires NodeJS, NPM, and MongoDB.
 Remote DB is for production use ONLY. Use a local mongodb for testing.
 
 #Running
-__Node JS Server__\s\s
+__Node JS Server__  
 on Mac: `> DEBUG=dork-api ./bin/www`
 
-__MongoDb Instance__\s\s
+__MongoDb Instance__  
 `> mongod`
 
 #Openshift info
 ##Jenkins
-https://jenkins-sababado.rhcloud.com/
-User:   `admin`\s\s
-Pwd:    `uberdork`\s\s
+https://jenkins-sababado.rhcloud.com/  
+User:   `admin`  
+Pwd:    `uberdork`  
 
 ##Dev
-Root User:  `admin`\s\s
-Root Pwd:   `arTF1_tXyu_S`\s\s
-Database Name:  `dork_db`\s\s
+Root User:  `admin`  
+Root Pwd:   `arTF1_tXyu_S`  
+Database Name:  `dork_db`  
 Connection URL: `mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/`
 
 #Files
--- config.js
+++ config.js
@@ -2,20 +2,23 @@ module.exports = function(){
     switch(process.env.NODE_ENV){
         case 'development':
             return {
-                "MONGO_URI": "mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/",
-                "NODE_PORT": "80"
+                "MONGO_URI": process.env.OPENSHIFT_MONGODB_DB_URL+"dork_db",
+                "NODE_PORT": process.env.OPENSHIFT_NODEJS_PORT,
+                "NODE_IP": process.env.OPENSHIFT_NODEJS_IP
             };
 
         case 'production':
             return {
-                "MONGO_URI": "mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/",
-                "NODE_PORT": "80"
+                "MONGO_URI": process.env.OPENSHIFT_MONGODB_DB_URL+"dork_db",
+                "NODE_PORT": process.env.OPENSHIFT_NODEJS_PORT,
+                "NODE_IP": process.env.OPENSHIFT_NODEJS_IP
             };
 
         default:
             return {
                 "MONGO_URI": "mongodb://localhost/dork_db",
-                "NODE_PORT": "3000"
+                "NODE_PORT": "3000",
+                "NODE_IP": '127.0.0.1'
             };
     }
 };
