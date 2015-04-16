df516206d518c6e07e5108d6cff4df51d20f000f
Robert Szabo <robert.james.szabo@gmail.com>
2015-04-15 19:11:53 -0500

    Adding API to sort a list of players by rank.

--- models/player.js
+++ models/player.js
@@ -12,7 +12,7 @@ var Schema = mongoose.Schema;
 var PlayerSchema = new Schema({
   firstName: { type: String, default: '', required: true},
   lastName: { type: String, default: '', required: true},
-  nickname: { type: String, default: '' },
+  nickname: { type: String, default: '' }
 });
 
 /**
@@ -48,7 +48,56 @@ PlayerSchema.method({
  */
 
 PlayerSchema.static({
+  arrContains: function(playerArr, player) {
+    if(!playerArr || !player) {
+      return false;
+    }
+    for(var i=0; i<playerArr.length; i++) {
+      if(playerArr[i]._id == player.id) {
+        return true;
+      }
+    }
+    return false;
+  },
 
+  getEmptyRankedPlayerObject: function(player) {
+    return {
+      player: player ? player : {},
+      totalPoints: 0,
+      gamesPlayed: 0,
+      rank: 0
+    };
+  },
+
+  /**
+   * Check if an array of Ranked Players contains a Player
+   * @param rankedPlayerArr
+   * @param player
+   * @returns {boolean} True if it contains the player, false if not.
+   */
+  rankedArrContains: function(rankedPlayerArr, player) {
+    if(!rankedPlayerArr || !player) {
+      return false;
+    }
+    for(var i=0; i<rankedPlayerArr.length; i++) {
+      if(rankedPlayerArr[i].player.id == player.id) {
+        return true;
+      }
+    }
+    return false;
+  },
+
+  getRankedPlayerAverage: function(gamePlayer) {
+    return gamePlayer.totalPoints / gamePlayer.gamesPlayed;
+  },
+
+  rankedPlayerSort: function(rankedPlayers) {
+    rankedPlayers.sort(function(a,b){
+      var aAvg = PlayerSchema.statics.getRankedPlayerAverage(a);
+      var bAvg = PlayerSchema.statics.getRankedPlayerAverage(b);
+      return bAvg - aAvg;
+    });
+  }
 });
 
 /**
--- routes/players.js
+++ routes/players.js
@@ -28,6 +28,16 @@ router.get('/', function (req, res, next) {
 	})
 });
 
+router.post('/sort/', function (req, res, next) {
+	var rankedPlayers = getRankedPlayers(req, next, function(rankedPlayers) {
+		var players = [];
+		for(var i=0; i<rankedPlayers.length; i++) {
+			players.push(rankedPlayers[i].player);
+		}
+		res.json(players);
+	});
+});
+
 /* POST save a player. */
 router.post('/', function (req, res, next) {
 	console.info("saving player: ", req.body);
@@ -53,6 +63,11 @@ router.post('/', function (req, res, next) {
  * Query params:
  * 	month - integer
  * 	(Optional) year - integer. Default value is the current year.
+ * 	sort - Type of sort to do. Default is by rank.
+ * 		- 0: rank - Sort by rank ascending.
+ * 		- 1: game - Game order. Example 1,3,5,7,9,10,8,6,4,2
+ * Post params:
+ *  playerArray - array of players. Get the ranks for only these players
  *
  * @return A list of ranked players.
  * <pre>
@@ -64,27 +79,30 @@ router.post('/', function (req, res, next) {
  *		}
  * 	</pre>
  */
-router.get('/ranked/', function (req, res, next) {
+router.post('/ranked/sort/', function(req, res, next) {
+	var rankedPlayers = getRankedPlayers(req, next, function(rankedPlayers) {
+		res.json(rankedPlayers);
+	});
+});
+router.get('/ranked/', function(req, res, next) {
+	var rankedPlayers = getRankedPlayers(req, next, function(rankedPlayers) {
+		res.json(rankedPlayers);
+	});
+});
+
+function getRankedPlayers(req, next, success) {
 	// Get date ranges
-	var now = new Date();
-	var year = req.query.year ? parseInt(req.query.year) : now.getUTCFullYear();
-	var month;
-	var endMonth;
-	if(req.query.month) {
-		month = parseInt(req.query.month);
-		endMonth = month+1;
-		if(endMonth > 11) endMonth = 0;
-	} else {
-		if(year) {
-			month = 0;
-			endMonth = 11;
-		}
-	}
-	var startDateRange = new Date(year, month, 1);
-	var endDateRange = new Date(year, endMonth, 1);
+	var dateRange = getDateRange(req);
+	var startDateRange = dateRange[0];
+	var endDateRange = dateRange[1];
 
 	// get all games that happened in the time span
-	GameModel.find()
+
+	var returnAllPlayers = true;
+	if (req.body && req.body.constructor === Array) {
+		returnAllPlayers = false;
+	}
+	query = GameModel.find()
 		.select('winner datePlayed players')
 		.where('datePlayed').gte(startDateRange).lt(endDateRange)
 		.populate('players')
@@ -106,81 +124,94 @@ router.get('/ranked/', function (req, res, next) {
 				} // end loop for game
 				//all ranked players are set at this point
 
+				if(!returnAllPlayers) {
+					// remove players not in the request body
+					for(var k=0; k<rankedPlayers.length; k++) {
+						if(!PlayerModel.arrContains(req.body, rankedPlayers[k].player)) {
+							// Not interested in this player, remove it from the array
+							rankedPlayers.splice(k--, 1);
+						}
+					}
+				}
+
 				// sort array by rank (points per games played)
-				rankedPlayers.sort(function(a,b){
-					var aAvg = getPlayerAverage(a);
-					var bAvg = getPlayerAverage(b);
-					return bAvg - aAvg;
-				})
+				PlayerModel.rankedPlayerSort(rankedPlayers);
 				// add rank value to each player so it is explicit
 				for(var i=0; i<rankedPlayers.length; i++) {
 					rankedPlayers[i].rank = i+1;
 				}
 			}
 
+			// Get array of all requested player ids
+			var requestedPlayerIds = null;
+			if(!returnAllPlayers) {
+				requestedPlayerIds = [];
+				for(var p=0; p<req.body.length; p++) {
+					requestedPlayerIds.push(req.body[p]._id);
+				}
+			}
+
 			// Now fetch the complete list of players and append any
 			// of them to the list if need be. These are the "un-ranked" players.
-			PlayerModel.find(function (err, players) {
-				if(err) return next(err);
+			var where = returnAllPlayers ? {} : {'_id': {$in: requestedPlayerIds}};
+			PlayerModel.find(where, function (err, players) {
+				if (err) return next(err);
 
-				for(var i=0; i<players.length; i++) {
-					if(!rankedPlayersArrContainsPlayer(rankedPlayers, players[i])) {
-						rankedPlayers.push(getEmptyRankedPlayerObject(players[i]));
+				for (var i = 0; i < players.length; i++) {
+					var player = players[i];
+					// If the player is not ranked then add it as an unranked player.
+					if (!PlayerModel.rankedArrContains(rankedPlayers, player)) {
+						rankedPlayers.push(PlayerModel.getEmptyRankedPlayerObject(player));
 					}
 				}
 
-				res.json(rankedPlayers);
+				success(rankedPlayers);
 			})
-
-			//res.json(rankedPlayers);
 		});
-});
+}
 
 /**
- * Check if an array of Ranked Players contains a Player
- * @param rankedPlayersArr
- * @param player
- * @returns {boolean} True if it contains the player, false if not.
+ * Get date ranges from a request object.
+ * @param req Should contain a month and optionally a year. Default is current year.
+ * @returns {*[]} Array containing the start date and end date.
  */
-var rankedPlayersArrContainsPlayer = function(rankedPlayersArr, player) {
-	for(var i=0; i<rankedPlayersArr.length; i++) {
-		if(rankedPlayersArr[i].player.id == player.id) {
-			return true;
+function getDateRange(req) {
+	var now = new Date();
+	var year = req.query.year ? parseInt(req.query.year) : now.getUTCFullYear();
+	var month;
+	var endMonth;
+	if(req.query.month) {
+		month = parseInt(req.query.month);
+		endMonth = month+1;
+		if(endMonth > 11) endMonth = 0;
+	} else {
+		if(year) {
+			month = 0;
+			endMonth = 11;
 		}
 	}
-	return false;
+	var startDateRange = new Date(year, month, 1);
+	var endDateRange = new Date(year, endMonth, 1);
+	return [startDateRange, endDateRange];
 }
 
-var getPlayerAverage = function(gamePlayer) {
-	return gamePlayer.totalPoints / gamePlayer.gamesPlayed;
-}
-
-var getEmptyRankedPlayerObject = function(player) {
-	return {
-		player: player ? player : {},
-		totalPoints: 0,
-		gamesPlayed: 0,
-		rank: 0
-	};
-}
-
-var getRankedPlayerObject = function(rankedPlayersArr, gamePlayer) {
+function getRankedPlayerObject(rankedPlayersArr, gamePlayer) {
 	// first look for an existing player. If found then update it.
 	for(var i=0; i < rankedPlayersArr.length; i++) {
 		var rankedPlayer = rankedPlayersArr[i];
 		if(rankedPlayer.player._id == gamePlayer.player._id) {
-			rankedPlayersArr[i] = addRankedPlayerObjects(rankedPlayer, gamePlayer);
+			rankedPlayersArr[i] = mergeRankedPlayerObjects(rankedPlayer, gamePlayer);
 			return rankedPlayersArr;
 		}
 	}
 	// No existing player was found so add a new one.
-	var newRankedPlayer = addRankedPlayerObjects(
-		getEmptyRankedPlayerObject(), gamePlayer);
+	var newRankedPlayer = mergeRankedPlayerObjects(
+		PlayerModel.getEmptyRankedPlayerObject(), gamePlayer);
 	rankedPlayersArr.push(newRankedPlayer);
 	return rankedPlayersArr;
 }
 
-var addRankedPlayerObjects = function(rp1, rp2) {
+function mergeRankedPlayerObjects(rp1, rp2) {
 	rp1.player = rp2.player;
 	rp1.totalPoints += rp2.points;
 	rp1.gamesPlayed++;
