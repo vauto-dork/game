88b24c1d836c416b222a67389072a42ee72f959c
Alex Tam <alex.tam@vauto.com>
2015-05-05 00:13:22 -0500

    Gets alphabetical list of all players.

--- public/directives/Playlist.js
+++ public/directives/Playlist.js
@@ -11,8 +11,7 @@ var Playlist = function() {
 }
 
 var PlaylistController = function ($scope, $http) {
-	
-	$http.get('/players/ranked').success(function(data, status, headers, config) {
+	$http.get('/players?sort=true').success(function(data, status, headers, config) {
     // this callback will be called asynchronously
     // when the response is available
     $scope.players = data;
@@ -22,16 +21,6 @@ var PlaylistController = function ($scope, $http) {
     // or server returns response with an error status.
     debugger;
   });
-
-  this.playerAverage = function(points, gamesPlayed){
-  	var average = 0;
-  	
-  	if(gamesPlayed > 0){
-  		average = points/gamesPlayed;
-  	}
-
-  	return average.toFixed(2);
-  };
 }
 
 PlaylistController.$inject = ['$scope', '$http'];
