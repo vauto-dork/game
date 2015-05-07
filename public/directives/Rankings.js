d4497a21a36d6b3be465c67a135569ac4b7b02ea
Alex Tam <alex.tam@vauto.com>
2015-05-07 00:32:11 -0500

    Added fluid grid to fill space when viewed on desktop. Fade out zero ranks. Removed 'New Guy' and 'New Girl' from roster. Moved fonts off Google to local. Removed normalize.css as it was redundant (bootstrap already includes it). Added config file to gitignore.

--- public/directives/Rankings.js
+++ public/directives/Rankings.js
@@ -15,13 +15,24 @@ var RankingsController = function ($scope, $http) {
 	$http.get('/players/ranked').success(function(data, status, headers, config) {
     // this callback will be called asynchronously
     // when the response is available
-    $scope.players = data;
+    $scope.players = data.filter(function (el) {
+                        // Removes 'New Guy' and 'New Girl'
+                        return el.player.firstName !== "New";
+                       });
   }).
   error(function(data, status, headers, config) {
     // called asynchronously if an error occurs
     // or server returns response with an error status.
     debugger;
   });
+
+  this.playerFullName = function(player) {
+    return player.firstName + ' ' + player.lastName;
+  }
+
+  this.playerInitials = function(player) {
+    return player.firstName.charAt(0) + player.lastName.charAt(0);
+  }
 
   this.playerAverage = function(points, gamesPlayed){
   	var average = 0;
@@ -32,6 +43,10 @@ var RankingsController = function ($scope, $http) {
 
   	return average.toFixed(2);
   };
+
+  this.hasNoRank = function(rank) {
+    return (rank <= 0) ? 'ranking-no-rank' : '';
+  }
 }
 
 RankingsController.$inject = ['$scope', '$http'];
