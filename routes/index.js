b11a4da1a17d08d4903b2077582e3733df5d8f62
Alex Tam <alex.tam@vauto.com>
2015-05-24 15:36:36 -0500

    Added "Dork" to title. Changed "Current Ranking" to "Leaderboard". Added spacing around icons on buttons.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Current Rankings' });
+  res.render('index', { title: 'Leaderboard' });
 });
 
 /* GET home page. */
