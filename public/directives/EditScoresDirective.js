fba0ad65bcf7e62c3b7d33aab6167d3c18d6b66b
Alex Tam <alex.tam@vauto.com>
2015-06-21 17:02:16 -0500

    Rank selector working. Needs clean-up.

--- public/directives/EditScoresDirective.js
+++ public/directives/EditScoresDirective.js
@@ -16,16 +16,28 @@ var EditScoresController = function ($scope, $http, playerNameFactory) {
 	me.pointsMin = -4;
 	me.pointsMax = 99;
 	
-	this.formatPlayer = function(player){
+	me.rankHandler = function(player) {
+		player.rank = player.rank === null ? 0 : player.rank;
+		
+		me.players.forEach(function(p) {
+			if(p._id !== player._id) {
+				if(player.rank > 0 && p.rank === player.rank) {
+					p.rank = 0;
+				}
+			}
+		});
+	};
+	
+	me.formatPlayer = function(player){
 		return playerNameFactory.playerNameFormat(player);	
 	};
 		
-	this.decrementScore = function(player) {
+	me.decrementScore = function(player) {
 		var points = parseInt(player.points);
 		player.points = (points - 1 >= me.pointsMin) ? points - 1 : points;
 	};
 	
-	this.incrementScore = function(player) {
+	me.incrementScore = function(player) {
 		var points = parseInt(player.points);
 		player.points = (points + 1 <= me.pointsMax) ? points + 1 : points;
 	};
