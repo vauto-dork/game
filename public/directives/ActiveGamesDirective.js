2add2e5504c7dabadf2e811f2ceb78b4399f2b91
Alex Tam <alex.tam@vauto.com>
2015-06-25 23:38:00 -0500

    Added state machine to 'Active Games' to handle show/hide of DOM elements and to handle calling the HTTP requests.

--- public/directives/ActiveGamesDirective.js
+++ public/directives/ActiveGamesDirective.js
@@ -7,20 +7,58 @@ var ActiveGamesDirective = function() {
 		controllerAs: 'ctrl',
 		bindToController: true
 	};
-}
+};
 
 var ActiveGamesController = function ($scope, $http, $window, playerNameFactory) {
 	var me = this;
-	me.loading = true;
-	me.activeGamePath = "/ActiveGames/json";
+	me.loading = false;
+	me.showNoGamesWarning = false;
+	me.showErrorMessage = false;
 	
-	me.scrollToTop = function() {
-		$window.scrollTo(0, 0);
+	me.activeGamePath = "/ActiveGames/json";
+	me.gameToDelete = null;
+	
+	me.State = {
+		Loading: 0,
+		NoGames: 1,
+		Loaded: 2,
+		Deleting: 3,
+		Error: 4
 	};
 	
+	me.changeState = function(newState) {
+		me.showErrorMessage = newState === me.State.Error;
+		me.showNoGamesWarning = newState === me.State.NoGames;
+		
+		switch(newState){
+			case me.State.Loading:
+				me.gameToDelete = null;
+				me.loading = true;
+				me.getGames();
+				break;
+			case me.State.NoGames:
+				me.loading = false;
+				break;
+			case me.State.Loaded:
+				me.loading = false;
+				break;
+			case me.State.Deleting:
+				me.loading = true;
+				me.delete();
+				break;
+		}
+	};
+	
+	// Dont call directly. Change state to "Loading" instead.
 	me.getGames = function() {
 		$http.get(me.activeGamePath)
 		.success(function(data, status, headers, config) {
+			if(!data || data.length === 0){
+				me.games = [];
+				me.changeState(me.State.NoGames);
+				return;
+			}
+			
 			me.games = data;
 			me.games.forEach(function(game){
 				game.deleteWarning = false;
@@ -28,27 +66,37 @@ var ActiveGamesController = function ($scope, $http, $window, playerNameFactory)
 					value.player = playerNameFactory.playerNameFormat(value.player);
 				});
 			});
-			me.loading = false;
+			
+			me.changeState(me.State.Loaded);
 		})
 		.error(function(data, status, headers, config) {
-			debugger;
+			me.changeState(me.State.Error);
+			console.error(data);
+		});
+	};
+	
+	// Dont call directly. Change state to "Deleting" instead.
+	me.delete = function() {
+		if(!me.gameToDelete) {
+			return;
+		}
+		
+		$http.delete(me.activeGamePath + '/' + me.gameToDelete._id)
+		.success(function(data, status, headers, config) {
+			me.changeState(me.State.Loading);
+		})
+		.error(function(data, status, headers, config) {
+			me.changeState(me.State.Error);
+		    console.error(data);
 		});
 	};
 	
 	me.deleteGame = function(game) {
-		$http.delete(me.activeGamePath + '/' + game._id)
-		.success(function(data, status, headers, config) {
-			game.deleteWarning = false;
-		    me.getGames();
-			me.scrollToTop();
-		})
-		.error(function(data, status, headers, config) {
-			game.deleteWarning = false;
-		    debugger;
-		});
+		me.gameToDelete = game;
+		me.changeState(me.State.Deleting);
 	};
 	
-	me.getGames();
+	me.changeState(me.State.Loading);
 };
 
 ActiveGamesController.$inject = ['$scope', '$http', '$window', 'playerNameFactory'];
