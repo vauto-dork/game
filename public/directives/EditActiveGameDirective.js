7d91c94d4f1965ab97a24556cc18c8642304365d
Alex Tam <alex.tam@vauto.com>
2015-06-27 15:55:48 -0500

    Changed $scope to me.

--- public/directives/EditActiveGameDirective.js
+++ public/directives/EditActiveGameDirective.js
@@ -11,33 +11,42 @@ var EditActiveGameDirective = function() {
 
 var EditActiveGameController = function ($scope, $http, $location, $window, playerNameFactory) {
 	var me = this;
-	$scope.loading = true;
-	$scope.errorLoading = false;
+	me.loading = true;
+	me.errorLoading = false;
 	me.datePlayedJs = new Date();
 	
 	$scope.alerts = [];
 	
+	me.State = {
+		Loading: 0,
+		Error: 1
+	};
+	
+	me.changeState = function(newState) {
+		
+	};
+	
 	if($location.path() !== undefined || $location.path() !== ''){
-		$scope.activeGamePath = '/ActiveGames/json' + $location.path();
+		me.activeGamePath = '/ActiveGames/json' + $location.path();
 	}
 	else {
-		$scope.activeGamePath = '';
+		me.activeGamePath = '';
 	}
 	
-	$http.get($scope.activeGamePath).success(function(data, status, headers, config) {
+	$http.get(me.activeGamePath).success(function(data, status, headers, config) {
 		if(data === null || data === undefined) {
-			me.addAlert('danger', 'Cannot find game.');
-			$scope.errorLoading = true;
+			$scope.addAlert('danger', 'Cannot find game.');
+			me.errorLoading = true;
 		}
 		else {
-			$scope.game = data;
+			me.game = data;
 			me.datePlayedJs = Date.parse(data.datePlayed);
 		}
 		
-		$scope.loading = false;
+		me.loading = false;
 	}).
 	error(function(data, status, headers, config) {
-		me.addAlert('danger', 'Cannot load game.');
+		$scope.addAlert('danger', 'Cannot load game.');
 		me.scrollToTop();
 	    debugger;
 	});
@@ -46,11 +55,11 @@ var EditActiveGameController = function ($scope, $http, $location, $window, play
 		$scope.alerts.splice(index, 1);
 	};
 	
-	me.addAlert = function(messageType, message) {
+	$scope.addAlert = function(messageType, message) {
 		$scope.alerts.push({type: messageType, msg: message});		
 	};
 		
-	me.clearAlerts = function() {
+	$scope.clearAlerts = function() {
 		$scope.alerts = [];	
 	};
 	
@@ -60,29 +69,29 @@ var EditActiveGameController = function ($scope, $http, $location, $window, play
 	
 	me.savePrep = function(){
 		// Convert datetime to string.
-		$scope.game.datePlayed = me.getFormattedDate();
+		me.game.datePlayed = me.getFormattedDate();
 		
 		// Convert blank points to zeroes.
-		$scope.game.players.forEach(function(player) { player.points = !player.points ? 0 : player.points; });
+		me.game.players.forEach(function(player) { player.points = !player.points ? 0 : player.points; });
 	};
 	
 	me.finalizeCheck = function() {
-		me.clearAlerts();
+		$scope.clearAlerts();
 		
-		var rank1 = $scope.game.players.filter(function(value) {return value.rank === 1;}).length;
-		var rank2 = $scope.game.players.filter(function(value) {return value.rank === 2;}).length;
-		var rank3 = $scope.game.players.filter(function(value) {return value.rank === 3;}).length;
+		var rank1 = me.game.players.filter(function(value) {return value.rank === 1;}).length;
+		var rank2 = me.game.players.filter(function(value) {return value.rank === 2;}).length;
+		var rank3 = me.game.players.filter(function(value) {return value.rank === 3;}).length;
 		
 		if(rank1 !== 1) {
-			me.addAlert('danger', 'No winner selected.');
+			$scope.addAlert('danger', 'No winner selected.');
 		}
 		
 		if(rank2 !== 1) {
-			me.addAlert('danger', 'No second place selected.');
+			$scope.addAlert('danger', 'No second place selected.');
 		}
 		
 		if(rank3 !== 1) {
-			me.addAlert('danger', 'No third place selected.');
+			$scope.addAlert('danger', 'No third place selected.');
 		}
 		
 		return (rank1 === 1 && rank2 === 1 && rank3 === 1);
@@ -98,15 +107,15 @@ var EditActiveGameController = function ($scope, $http, $location, $window, play
 	};
 	
 	me.saveGame = function() {
-		me.clearAlerts();
+		$scope.clearAlerts();
 		me.savePrep();
 		
-		$http.put($scope.activeGamePath, $scope.game).success(function(data, status, headers, config) {
-		    me.addAlert('success', 'Game saved successfully!');
+		$http.put(me.activeGamePath, me.game).success(function(data, status, headers, config) {
+		    $scope.addAlert('success', 'Game saved successfully!');
 			me.scrollToTop();
 		}).
 		error(function(data, status, headers, config) {
-		    me.addAlert('danger', 'Game save failure!');
+		    $scope.addAlert('danger', 'Game save failure!');
 			me.scrollToTop();
 			debugger;
 		});
@@ -118,28 +127,28 @@ var EditActiveGameController = function ($scope, $http, $location, $window, play
 			return;
 		}
 		
-		var winner = $scope.game.players.filter(function(value) {return value.rank === 1; });
+		var winner = me.game.players.filter(function(value) {return value.rank === 1; });
 		
-		$scope.game.winner = { _id: winner[0].player._id };
+		me.game.winner = { _id: winner[0].player._id };
 		
 		me.savePrep();
 		
-		$http.post('/games', $scope.game).success(function(data, status, headers, config) {
+		$http.post('/games', me.game).success(function(data, status, headers, config) {
 		    me.deleteGame();
 		}).
 		error(function(data, status, headers, config) {
-			me.addAlert('danger', 'Cannot finalize game!');
+			$scope.addAlert('danger', 'Cannot finalize game!');
 			me.scrollToTop();
 		    debugger;
 		});
 	};
 	
 	me.deleteGame = function() {
-		$http.delete($scope.activeGamePath).success(function(data, status, headers, config) {
+		$http.delete(me.activeGamePath).success(function(data, status, headers, config) {
 		    $window.location.href = '/GameHistory';
 		}).
 		error(function(data, status, headers, config) {
-			me.addAlert('danger', 'Cannot finalize game!');
+			$scope.addAlert('danger', 'Cannot finalize game!');
 			me.scrollToTop();
 		    debugger;
 		});
