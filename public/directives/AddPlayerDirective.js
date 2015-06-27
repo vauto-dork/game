d1744927c136701bb59b2ad63947b65ea4767481
Alex Tam <alex.tam@vauto.com>
2015-06-27 15:07:40 -0500

    Added state machine to 'Add Player' to disable controls during save.

--- public/directives/AddPlayerDirective.js
+++ public/directives/AddPlayerDirective.js
@@ -7,41 +7,67 @@ var AddPlayerDirective = function() {
 		controllerAs: 'ctrl',
 		bindToController: true
 	};
-}
+};
 
 var AddPlayerController = function ($scope, $timeout, $http) {
 	var me = this;
 	me.success = false;
 	me.failure = false;
+	me.disableControls = false;
 	
-	me.reset = function () {
+	me.State = {
+		Ready: 0,
+		Saving: 1,
+		Saved: 2,
+		Fail: 3
+	};
+	
+	me.changeState = function(newState) {
+		me.success = newState === me.State.Saved;
+		me.failure = newState === me.State.Fail;
+		me.disableControls = newState === me.State.Saving;
+		
+		switch(newState) {
+			case me.State.Ready:
+				me.resetForm();
+				break;
+			case me.State.Saving:
+				me.savePlayer();
+				break;
+			case me.State.Saved:
+				me.resetForm();
+				$timeout(function(){
+					me.changeState(me.State.Ready);
+				}, 5000);
+				break;				
+		}
+	};
+	
+	me.resetForm = function() {
 		me.firstName = '';
 		me.lastName = '';
 		me.nickname = '';
-		me.success = false;
-		me.failure = false;
+	};
+	
+	me.savePlayer = function() {
+		$http.post('/players', { firstName: me.firstName, lastName: me.lastName, nickname: me.nickname })
+			.success(function (data, status, headers, config) {
+				me.changeState(me.State.Saved);
+			}).error(function (data, status, headers, config) {
+				me.changeState(me.State.Fail);
+				console.error(data);
+		});
+	};
+	
+	me.reset = function () {
+		me.changeState(me.State.Ready);
 	};
 	
 	me.submit = function(){
-		me.success = false;
-		me.failure = false;
-		
-		$http.post('/players', { firstName: me.firstName, lastName: me.lastName, nickname: me.nickname })
-			.success(function (data, status, headers, config) {
-				me.firstName = '';
-				me.lastName = '';
-				me.nickname = '';
-				me.success = true;
-				
-				$timeout(function(){
-					me.success = false;
-				}, 5000);
-				
-			}).error(function (data, status, headers, config) {
-				me.failure = true;
-				debugger;
-		});
+		me.changeState(me.State.Saving);
 	};
+	
+	me.changeState(me.State.Ready);
 };
 
 AddPlayerController.$inject = ['$scope', '$timeout', '$http'];
