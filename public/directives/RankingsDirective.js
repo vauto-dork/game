d6bd821501de0e4bbad5de40e986895db93bfc2a
Alex Tam <alex.tam@vauto.com>
2015-07-03 16:53:08 -0500

    Changed rankings to use state machine and added month and year attributes.

--- public/directives/RankingsDirective.js
+++ public/directives/RankingsDirective.js
@@ -1,36 +1,61 @@
 var RankingsDirective = function() {
-	return {
-		scope: {
-
-		},
-		templateUrl: '/directives/RankingsTemplate.html',
-		controller: 'RankingsController',
-		controllerAs: 'ctrl',
-		bindToController: true
-	};
-}
+  return {
+    scope: {
+      month: "=",
+      year: "="  
+    },
+    templateUrl: '/directives/RankingsTemplate.html',
+    controller: 'RankingsController',
+    controllerAs: 'ctrl',
+    bindToController: true
+  };
+};
 
 var RankingsController = function ($scope, $http, playerNameFactory) {
-  $scope.playersLoading = true;
-  var currentMonth = new Date().getMonth();
-  var rankedUrl = '/players/ranked?month=' + currentMonth;
+  var me = this;
+  me.showLoading = true;
+  me.showRankings = false;
+  me.players = [];
+  
+  me.State = {
+		Loading: 0,
+		Loaded: 1,
+		Error: 2
+	};
 	
-	$http.get(rankedUrl).success(function(data, status, headers, config) {
-    // this callback will be called asynchronously
-    // when the response is available
-    $scope.players = data;
-    $scope.players.forEach(function(value){
-      value.player = playerNameFactory.playerNameFormat(value.player);
+	me.changeState = function(newState) {
+    me.showLoading = newState === me.State.Loading;
+    me.showRankings = newState === me.State.Loaded;
+		me.showErrorMessage = newState === me.State.Error;
+		
+		switch(newState){
+			case me.State.Loading:
+				me.getRankings();
+				break;
+		}
+	};
+  
+  me.getRankings = function(){
+    var mon = me.month === undefined ? new Date().getMonth() : me.month;
+    var yr = me.year === undefined ? new Date().getFullYear() : me.year;
+    
+    var rankedUrl = '/players/ranked?month=' + mon + '&year=' + yr;
+  	
+  	$http.get(rankedUrl)
+    .success(function(data, status, headers, config) {
+      me.players = data;
+      me.players.forEach(function(value){
+        value.player = playerNameFactory.playerNameFormat(value.player);
+      });
+      me.changeState(me.State.Loaded);
+    })
+    .error(function(data, status, headers, config) {
+      me.changeState(me.State.Error);
+			console.error(data);
     });
-    $scope.playersLoading = false;
-  }).
-  error(function(data, status, headers, config) {
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
-    debugger;
-  });
-
-  this.playerAverage = function(points, gamesPlayed){
+  };
+	
+  me.playerAverage = function(points, gamesPlayed){
   	var average = 0;
   	
   	if(gamesPlayed > 0){
@@ -40,10 +65,12 @@ var RankingsController = function ($scope, $http, playerNameFactory) {
   	return average.toFixed(2);
   };
 
-  this.hasNoRank = function(rank) {
+  me.hasNoRank = function(rank) {
     return (rank <= 0) ? 'ranking-no-rank' : '';
-  }
-}
+  };
+  
+  me.changeState(me.State.Loading);
+};
 
 RankingsController.$inject = ['$scope', '$http', 'playerNameFactory'];
 
