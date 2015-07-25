5eee15ac32caa9ea34a733a6b9faa01113699289
Alex Tam <alex.tam@vauto.com>
2015-07-24 23:06:15 -0500

    Created a game card directive and replaced game cards in active games.

--- public/directives/ActiveGamesDirective.js
+++ public/directives/ActiveGamesDirective.js
@@ -9,7 +9,7 @@ var ActiveGamesDirective = function() {
 	};
 };
 
-var ActiveGamesController = function ($scope, $http, $window, playerNameFactory) {
+var ActiveGamesController = function ($scope, $http, playerNameFactory) {
 	var me = this;
 	me.loading = false;
 	me.showNoGamesWarning = false;
@@ -17,34 +17,22 @@ var ActiveGamesController = function ($scope, $http, $window, playerNameFactory)
 	
 	me.activeGamePath = "/ActiveGames/json";
 	me.errorMessage = '';
-	me.selectedGame = null;
 	
 	me.State = {
 		Loading: 0,
 		NoGames: 1,
 		Loaded: 2,
-		Deleting: 3,
-		Copy: 4,
-		Error: 5
+		Error: 3
 	};
 	
 	me.changeState = function(newState) {
-		me.loading = newState === me.State.Loading ||
-					 newState === me.State.Deleting ||
-					 newState === me.State.Copy;
+		me.loading = newState === me.State.Loading;
 		me.showErrorMessage = newState === me.State.Error;
 		me.showNoGamesWarning = newState === me.State.NoGames;
 		
 		switch(newState){
 			case me.State.Loading:
-				me.selectedGame = null;
 				me.getGames();
-				break;
-			case me.State.Copy:
-				me.copy();
-				break;
-			case me.State.Deleting:
-				me.delete();
 				break;
 		}
 	};
@@ -80,52 +68,14 @@ var ActiveGamesController = function ($scope, $http, $window, playerNameFactory)
 		});
 	};
 	
-	// Dont call directly. Change state to "Deleting" instead.
-	me.delete = function() {
-		if(!me.selectedGame) {
-			me.errorHandler(null, 'No game selected!');
-			return;
-		}
-		
-		$http.delete(me.activeGamePath + '/' + me.selectedGame._id)
-		.success(function(data, status, headers, config) {
-			me.changeState(me.State.Loading);
-		})
-		.error(function(data, status, headers, config) {
-			me.errorHandler(data, 'Error deleting game!');
-		});
-	};
-	
-	// Dont call directly. Change state to "Copy" instead.
-	me.copy = function() {
-		if(!me.selectedGame) {
-			me.errorHandler(null, 'No game selected!');
-			return;
-		}
-		
-		$http.post('/activeGames/save', { players: me.selectedGame.players })
-		.success(function(data, status, headers, config) {
-			$window.location.href = '/activeGames/edit/#/' + data._id;
-		})
-		.error(function(data, status, headers, config) {
-			me.errorHandler(data, 'Error copying game!');
-		});
-	};
-	
-	me.deleteGame = function(game) {
-		me.selectedGame = game;
-		me.changeState(me.State.Deleting);
-	};
-	
-	me.copyGame = function(game) {
-		me.selectedGame = game;
-		me.changeState(me.State.Copy);
+	me.reload = function() {
+		me.changeState(me.State.Loading);
 	};
 	
 	me.changeState(me.State.Loading);
 };
 
-ActiveGamesController.$inject = ['$scope', '$http', '$window', 'playerNameFactory'];
+ActiveGamesController.$inject = ['$scope', '$http', 'playerNameFactory'];
 
 DorkModule.controller('ActiveGamesController', ActiveGamesController);
 DorkModule.directive('activeGames', ActiveGamesDirective);
