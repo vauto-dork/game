2137bece1dd0ec7c9e128ed995335414d2138f9a
Alex Tam <alex.tam@vauto.com>
2015-08-04 22:32:47 -0500

    Added month/year picker to game history.

--- public/directives/GameHistoryDirective.js
+++ public/directives/GameHistoryDirective.js
@@ -9,31 +9,51 @@ var GameHistoryDirective = function() {
 	};
 }
 
-var GameHistoryController = function ($scope, $http, playerNameFactory) {
+var GameHistoryController = function ($scope, $http, $timeout, playerNameFactory, monthYearQueryFactory) {
 	var me = this;
+	me.month = new Date().getMonth();
+	me.year = new Date().getFullYear();
+	
 	me.loading = true;
 	me.errorMessage = '';
 	me.gamePath = "/Games";
 	
 	me.State = {
-		Loading: 0,
-		NoGames: 1,
-		Loaded: 2,
-		Error: 3
+		Init: 0,
+		Loading: 1,
+		NoGames: 2,
+		Ready: 3,
+		Error: 4,
+		Change: 5,
 	};
 	
 	me.changeState = function(newState) {
-		me.loading = newState === me.State.Loading;
+		me.loading = newState === me.State.Init
+				  || newState === me.State.Change
+				  || newState === me.State.Loading;
 		me.showErrorMessage = newState === me.State.Error;
 		me.showNoGamesWarning = newState === me.State.NoGames;
 		
 		switch(newState){
+			case me.State.Init:
+				$timeout(function() {
+					me.month = monthYearQueryFactory.GetMonthQueryParam(me.month);
+					me.year = monthYearQueryFactory.GetYearQueryParam(me.year);
+					me.changeState(me.State.Loading);
+				}, 0);
+				break;
+			case me.State.Change:
+				$timeout(function() {
+					monthYearQueryFactory.SaveQueryParams(me.month, me.year);
+					me.changeState(me.State.Loading);
+				}, 0);
+				break;
 			case me.State.Loading:
 				me.getGames();
 				break;
 			case me.State.NoGames:
 				break;
-			case me.State.Loaded:
+			case me.State.Ready:
 				break;
 			case me.State.Deleting:
 				me.delete();
@@ -49,28 +69,39 @@ var GameHistoryController = function ($scope, $http, playerNameFactory) {
 	
 	// Dont call directly. Change state to "Loading" instead.
 	me.getGames = function() {
-		$http.get(me.gamePath).success(function(data, status, headers, config) {
-		    me.games = data;
+		var path = me.gamePath + '?month=' + me.month + '&year=' + me.year;
+		$http.get(path).success(function(data, status, headers, config) {
+			me.games = data;
+			
+			if(data.length === 0) {
+				me.changeState(me.State.NoGames)
+				return;
+			}
+			
 			me.games.forEach(function(game){
 				game.players.forEach(function(value){
 			      value.player = playerNameFactory.playerNameFormat(value.player);
 			    });
 			});
-			me.loading = false;
+			me.changeState(me.State.Ready);
 		}).
 		error(function(data, status, headers, config) {
 		    me.errorHandler(data, 'Error loading games!');
 		});
 	};
 	
+	me.updateQueryParams = function() {
+		me.changeState(me.State.Change);
+	};
+	
 	me.reload = function() {
 		me.changeState(me.State.Loading);
 	};
 	
-	me.changeState(me.State.Loading);
+	me.changeState(me.State.Init);
 };
 
-GameHistoryController.$inject = ['$scope', '$http', 'playerNameFactory'];
+GameHistoryController.$inject = ['$scope', '$http', '$timeout', 'playerNameFactory', 'monthYearQueryFactory'];
 
 DorkModule.controller('GameHistoryController', GameHistoryController);
 DorkModule.directive('gameHistory', GameHistoryDirective);
