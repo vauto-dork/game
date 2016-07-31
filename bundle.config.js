29fefafa8729a5002f1ba0db5e157ecb4c3dd4ad
Alex Tam <alex.tam@vauto.com>
2016-07-31 10:32:57 -0500

    Centralized the file paths for bundling.

--- bundle.config.js
+++ bundle.config.js
@@ -1,3 +1,5 @@
+var bundleFiles = require('./public/bundles/bundleFiles.json');
+
 // Paths for file generation
 var generatedFilePath = './generated/';
 var pageModuleFilePath = './public/bundles/pageModules/';
@@ -18,103 +20,33 @@ var getScriptsArray = function(scriptPathArray, pageModuleFile) {
 };
 
 //-----------------------------------------------------------------------------
-var dorkAppBundleOptions = {
-  useMin: false,
-  uglify: false,
-  rev: false
+var getScriptConfig = function(pageModuleName) {
+  var pageModuleFile = pageModuleName === 'shared' ? null : (pageModuleName + '.js');
+  var scripts = bundleFiles.scripts[pageModuleName];
+
+  return {
+    scripts: getScriptsArray(scripts, pageModuleFile),
+    options: {
+      useMin: false,
+      uglify: false,
+      rev: false
+    }
+  };
 };
 
 //-----------------------------------------------------------------------------
 // Scripts must be listed in the order it should be concatenated.
 module.exports = {
   bundle: {
-    shared: {
-      scripts: 
-        getScriptsArray([
-          'shared/models/*.js',
-          'shared/services/*.js',
-          'shared/directives/*.js',
-          'shared/modules/*.js'
-        ], null),
-      options: dorkAppBundleOptions
-    },
-    activeGames: {
-      scripts: 
-        getScriptsArray([
-          'areas/activeGame/directives/ActiveGamesDirective.js'
-        ], 'activeGames.js'),
-      options: dorkAppBundleOptions
-    },
-    addPlayer: {
-      scripts: 
-        getScriptsArray([
-          'areas/players/directives/PlayerFormDirective.js',
-          'areas/players/directives/AddPlayerDirective.js'
-        ], 'addPlayer.js'),
-      options: dorkAppBundleOptions
-    },
-    admin: {
-      scripts: getScriptsArray([], 'admin.js'),
-      options: dorkAppBundleOptions
-    },
-    createGame: {
-      scripts: getScriptsArray([
-        'areas/createGame/services/CreateGameService.js',
-        'areas/createGame/directives/ButtonsPanelDirective.js',
-        'areas/createGame/directives/SelectedPlayersDirective.js',
-        'areas/createGame/directives/CreateGameDirective.js'
-      ], 'createGame.js'),
-      options: dorkAppBundleOptions
-    },
-    editActiveGame: {
-      scripts: 
-        getScriptsArray([
-          'areas/editActiveGame/services/EditActiveGameService.js',
-          'areas/editActiveGame/directives/EditScoresDirective.js',
-          'areas/editActiveGame/directives/ReorderPlayersDirective.js',
-          'areas/editActiveGame/directives/ModifyPlayersDirective.js',
-          'areas/editActiveGame/directives/RevertFinalizeDirective.js',
-          'areas/editActiveGame/directives/EditActiveGameDirective.js'
-        ], 'editActiveGame.js'),
-      options: dorkAppBundleOptions
-    },
-    gameHistory: {
-      scripts: getScriptsArray([
-        'areas/history/directives/GameHistoryDirective.js'
-      ], 'gameHistory.js'),
-      options: dorkAppBundleOptions
-    },
-    index: {
-      scripts: 
-        getScriptsArray([
-          'areas/dotm/directives/DotmDirective.js',
-          'areas/dotm/modules/DotmModule.js',
-          'areas/rankings/services/RankingsService.js',
-          'areas/rankings/directives/RankingsCardDirective.js',
-          'areas/rankings/directives/RankingsDirective.js',
-          'areas/rankings/modules/RankingsModule.js',
-          'areas/rankings/directives/LeaderboardDirective.js'
-        ], 'index.js'),
-      options: dorkAppBundleOptions
-    },
-    playersList: {
-      scripts: getScriptsArray([
-        '/areas/players/directives/PlayerFormDirective.js',
-        '/areas/players/directives/PlayersListDirective.js'
-      ], 'playersList.js'),
-      options: dorkAppBundleOptions
-    },
-    rankingHistory: {
-      scripts: getScriptsArray([
-        'areas/dotm/directives/DotmDirective.js',
-        'areas/dotm/modules/DotmModule.js',
-        'areas/rankings/services/RankingsService.js',
-        'areas/rankings/directives/RankingsCardDirective.js',
-        'areas/rankings/directives/RankingsDirective.js',
-        'areas/rankings/modules/RankingsModule.js',
-        'areas/history/directives/RankingHistoryDirective.js',
-      ], 'rankingHistory.js'),
-      options: dorkAppBundleOptions
-    }
+    shared: getScriptConfig('shared'),
+    activeGames: getScriptConfig('activeGames'),
+    addPlayer: getScriptConfig('addPlayer'),
+    admin: getScriptConfig('admin'),
+    createGame: getScriptConfig('createGame'),
+    editActiveGame: getScriptConfig('editActiveGame'),
+    gameHistory: getScriptConfig('gameHistory'),
+    index: getScriptConfig('index'),
+    playersList: getScriptConfig('playersList'),
+    rankingHistory: getScriptConfig('rankingHistory')
   }
 };
