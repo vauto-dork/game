dd2bbe77d9bb49cc64611270118399f25cb875ca
Alex Tam <alex.tam@vauto.com>
2016-08-26 10:50:04 -0500

    Added recursive component bundling.

--- public/bundles/bundle.helper.js
+++ public/bundles/bundle.helper.js
@@ -4,27 +4,66 @@ var bundleFiles = require('./bundle.files.json');
 var generatedFilePath = './' + bundleFiles.sourceRootFilePath + '/';
 
 //-----------------------------------------------------------------------------
-var squashScriptFiles = function(pageModuleName) {
-    var scripts = bundleFiles.scripts[pageModuleName].files;
-    var components = bundleFiles.scripts[pageModuleName].components;
+var getComponent = function(componentName) {
+    return bundleFiles.components[componentName];
+}
 
-    if (components) {
-        var newList;        
-        for (var i = 0; i < components.length; i++) {
-            var items = bundleFiles.components[components[i]];
+//-----------------------------------------------------------------------------
+var prepend = function(dest, source) {
+    for(var i = 0; i < source.length; i++) {
+        var item = source[i];
+        var foundIndex = dest.indexOf(item); 
+        if(foundIndex >= 0) {
+            dest.splice(foundIndex, 1);
+            dest.splice(0, 0, item);
+        }
+    }
+}
+
+//-----------------------------------------------------------------------------
+var append = function(dest, source) {
+    for(var i = 0; i < source.length; i++) {
+        var item = source[i];
+        var foundIndex = dest.indexOf(item); 
+        if(foundIndex === -1) {
+            dest.push(item);
+        }
+    }
+}
+
+//-----------------------------------------------------------------------------
+var recursiveComponentSquash = function(scripts, componentsList) {
+    if (componentsList) {
+        for (var i = 0; i < componentsList.length; i++) {
+            var componentName = componentsList[i];
+            var componentFiles = getComponent(componentName).files;
+
+            var recursiveResults = recursiveComponentSquash(scripts, getComponent(componentName).components);
+
+            if(recursiveResults) {
+                prepend(scripts, recursiveResults);
+            }            
 
             if (i === 0) {
-                newList = items;
+                newList = componentFiles;
             } else {
                 // concat doesn't work here.
                 // see: http://stackoverflow.com/questions/16679565/why-cant-i-concat-an-array-reference-in-javascript
-                newList.push.apply(newList, items);
+                append(newList, componentFiles);
             }
         }
         scripts = !newList ? scripts : newList.concat(scripts);
     }
 
     return scripts;
+}
+
+//-----------------------------------------------------------------------------
+var squashScriptFiles = function(pageModuleName) {
+    var scripts = bundleFiles.scripts[pageModuleName].files;
+    var components = bundleFiles.scripts[pageModuleName].components;
+
+    return recursiveComponentSquash(scripts, components);
 }
 
 //-----------------------------------------------------------------------------
