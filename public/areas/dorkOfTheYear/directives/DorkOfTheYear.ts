310876e61859ef414ce2cc42f569d62b62c5bbaa
Alex Tam <alext9586@users.noreply.github.com>
2018-02-11 17:52:48 -0600

    Added note about how the winner is calculated. Added query params. Added state machine to main container.

--- public/areas/dorkOfTheYear/directives/DorkOfTheYear.ts
+++ public/areas/dorkOfTheYear/directives/DorkOfTheYear.ts
@@ -6,22 +6,52 @@ module DorkOfTheYear {
 		};
     }
 
+    enum State {
+		Init,
+		Ready,
+		Change
+	};
+
     export class DorkOfTheYearController {
-        public static $inject: string[] = ['$timeout', 'dateTimeService', 'dotyService'];
+        public static $inject: string[] = ['$timeout', 'monthYearQueryService', 'dateTimeService', 'dotyService'];
         
         private year: number;
 
-        constructor(private $timeout: ng.ITimeoutService, private dateTimeService: Shared.IDateTimeService, private dotyService: IDotyService) {
-            this.year = this.dateTimeService.currentYear();
-            this.dotyService.changeDate(this.year);
+        constructor(private $timeout: ng.ITimeoutService,
+            private monthYearQueryService: Shared.IMonthYearQueryService,
+            private dateTimeService: Shared.IDateTimeService,
+            private dotyService: IDotyService) {
+
+            this.changeState(State.Init);
         }
 
-        private updateQueryParams() {
-            // Timeouts are required to force a digest cycle so the query
+        private changeState(newState: State) {
+			// Timeouts are required to force a digest cycle so the query
 			// param factory will update in the correct scope.
-            this.$timeout(()=>{
-                this.dotyService.changeDate(this.year);
-            }, 0);
+			switch (newState) {
+				case State.Init:
+					this.$timeout(() => {
+						var date = this.monthYearQueryService.getQueryParams();
+						if(date) {
+							this.year = date.year;
+						} else {
+							this.year = this.dateTimeService.currentYear();
+						}
+						this.changeState(State.Change);
+					}, 0);
+					break;
+				case State.Change:
+					this.$timeout(() => {
+						this.monthYearQueryService.saveQueryParams(null, this.year);
+						this.dotyService.changeDate(this.year);
+					}, 0);
+					this.changeState(State.Ready);
+					break;
+			}
+		}
+
+		private updateQueryParams() {
+			this.changeState(State.Change);
 		}
     }
 }
